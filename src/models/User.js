6374b86f89c2b9b961a7d1c7d38b99f6f57a1887
Manoj <manojmishra571980@gmail.com>
2018-04-08 17:29:24 +1000

    jwt installed and working

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const schema = new mongoose.Schema(
 {
@@ -9,5 +10,11 @@ const schema = new mongoose.Schema(
 
 schema.methods.isValidPassword = function isValidPassword(password) 
 {    return bcrypt.compareSync(password, this.passwordHash);   };
+
+schema.methods.generateJWT = function generateJWT() 
+{  return jwt.sign(  {   email: this.email  }, "secretkey"    );  };
+
+schema.methods.toAuthJSON = function toAuthJSON() 
+{  return {  email: this.email,  token: this.generateJWT()  };    };
 
 export default mongoose.model('User', schema)
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -9,9 +9,10 @@ router.post("/", (req, res) =>
     const { credentials } = req.body;
     console.log('credentials',req.body);
     User.findOne({ email: credentials.email }).then(user => 
-    {  if (user && user.isValidPassword(credentials.password)) 
+    {  if (user && user.isValidPassword(credentials.password))   //isvalid is defined in model user
         { //console.log('user found',credentials);
-          res.json({ user: {email: user.email} });
+        // res.json({ user: {email: user.email} });
+          res.json({ user: user.toAuthJSON() });
          } 
       else { // console.log('user not found', credentials); //this will come to the console where server npm start is written
         res.status(400).json({ errors: { global: "Invalid credentials" } });
