d9d602e777a6b593b8272b01730fb0ba69e075ab
manojkmishra <manojkm1980@gmail.com>
2018-04-13 17:17:21 +1000

    selectBook working but pages not coming

--- src/routes/books.js
+++ src/routes/books.js
@@ -1,21 +1,49 @@
 import express from "express";
-const router = express.Router();
+import authenticate from "../middlewares/authenticate";
 
-router.get("/search", (req, res) => {
-    res.json({   books: [  {  goodreadsId: 1,  title: "aaa",  authors: "author a",
-                                covers: [  "https://images.gr-assets.com/books/1348990566l/5470.jpg",
-                                          "https://images.gr-assets.com/books/1504611957l/9577857.jpg"
-                                        ],
-                                pages: 3938
-                            },
-                            { goodreadsId: 2,  title: "bbb", authors: "author b",
-                                covers: [  "https://images.gr-assets.com/books/1392791656l/4921.jpg",
-                                            "https://images.gr-assets.com/books/1312036878l/627830.jpg"
-                                        ],
-                                pages: 2536
-                            }
-                        ]
-            });
-   });
+import request from "request-promise";
+import { parseString } from "xml2js";
+
+const router = express.Router();
+router.use(authenticate);
+
+
+router.get("/search", (req, res) => 
+{   console.log('req.query.q=',req.query.q);
+    request.get('https://www.goodreads.com/search/index.xml?key=JZivw0ci0JrxYxUteeF8g&q='+req.query.q)
+   // request.get('https://www.goodreads.com/search/index.xml?key=JZivw0ci0JrxYxUteeF8g&q=t')
+          // .then(result =>res.json({result}));
+           //.then(result =>parseString(result, (err, goodreadsResult) =>res.json({goodreadsResult})));
+           .then(result =>
+            parseString(result, (err, goodreadsResult) =>
+                            res.json({ books: goodreadsResult.GoodreadsResponse.search[0].results[0].work.map(
+                                        work => ({ goodreadsId: work.best_book[0].id[0]._,
+                                                    title: work.best_book[0].title[0],
+                                                    authors: work.best_book[0].author[0].name[0],
+                                                    covers: [work.best_book[0].image_url[0]]
+                                                    })
+                                            )
+                                    })
+                        )
+                )
+ });
+      //});
+    //  });
+
+    // res.json({   books: [  {  goodreadsId: 1,  title: "aaa",  authors: "author a",
+    //                             covers: [  "https://images.gr-assets.com/books/1348990566l/5470.jpg",
+    //                                       "https://images.gr-assets.com/books/1504611957l/9577857.jpg"
+    //                                     ],
+    //                             pages: 3938
+    //                         },
+    //                         { goodreadsId: 2,  title: "bbb", authors: "author b",
+    //                             covers: [  "https://images.gr-assets.com/books/1392791656l/4921.jpg",
+    //                                         "https://images.gr-assets.com/books/1312036878l/627830.jpg"
+    //                                     ],
+    //                             pages: 2536
+    //                         }
+    //                     ]
+    //         });
+  // });
 
    export default router;
